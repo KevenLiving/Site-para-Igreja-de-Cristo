3869bc7ac6d60f3955563efcb3d618f4758a9537
KevenLiving <l.kevenmedeiros.c@gmail.com>
2025-09-27 13:36:36 -0300

    Implementação das bibliotecas de segurança Bleach e Logging

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Essa biblioteca é nativa do Python, e permite interagir com o sistema operacional. Você pode entrar, criar, editar ou remover pastas ou arquivos. Ela é uma biblioteca maravilhosa e 
+# sem ela não teriamos acesso ao sistema operacional do nosso computador. 
 import os
 from flask import Flask
 from models.database import init_db
@@ -19,57 +21,60 @@ from flask_talisman import Talisman
 app = Flask(__name__)
 app.secret_key = 'Abracadabra' # Configurando a chave secreta
 
-# Para verificar se o sistema está ativo ou em produção
+# Para verificar se o sistema está em fase de desenvolvimento ou em produção
 IS_PRODUCTION = os.getenv('FLASK_ENV') == 'production' 
 
 
-# Configurando a Talisman de maneira condicional ao ambiente de produção. Caso contrário, ele bloqueia o prosseguimento do trabalho em localhost
-
+# Configurando a Talisman 
 if IS_PRODUCTION:
+    # De modo mais didático, é como uma a construção de uma casa. Quando ela está pronta, colocamos todos os métodos de segurança nela:
+    # Grane, cerca elétrica, alarme... mas quando está em reforma ou em construção, deixamos ela "desprotegida" para facilitar a vida
+    # dos trabalhadores. 
+    # IS_PRODUCTION ativa a segurança máxima no sistema que está em funcionamento ativo. 
     Talisman(app, 
-            force_https = True,
-            strict_transport_security=True:, # Genialmente, força ps navegadores a usar HTTPS, mesmo que um usuário qualquer tente acessar usando HTTP
+            force_https = True, # Obriga a usar método seguro para realizar requisições ao servidor
+            strict_transport_security=True, # Mesmo que alguém tente usar outro método, ele é redirecionado para o protocolo HTTPS
             strict_transport_security_max_age=31536000,  # 1 ano de proteção contado em segundos
-            strict_transport_security_include_subdomains=True,
-            content_security_policy={
-                'default-src': "'self'",
-                'script-src': "'self'",  # Sem unsafe-inline em produção
-                'style-src': "'self' 'unsafe-inline'",
-                'img-src': "'self' data:",
-                'font-src': "'self'",
-                'connect-src': "'self'",
-                'object-src': "'none'",
-                'base-uri': "'self'",
-                'frame-ancestors': "'none'"
+            strict_transport_security_include_subdomains=True, # Proteje subdominios além da rota principal 
+            content_security_policy={ # Aqui define a regra de quais recursos podem ser carregados 
+                'default-src': "'self'", # Somente aceita recursos do proprio servidor
+                'script-src': "'self'",  # Só scripts internos, não aceita scripts soltos pela internet, evitando ataques XSS
+                'style-src': "'self' 'unsafe-inline'", # Aceita CSS inline porque não oferece perigo
+                'img-src': "'self' data:", # Aceita imagens próprias e em formato Base64(diretamente no html precisar carregá-las)
+                'font-src': "'self'", # Fontes de letras somente vindas do meu servidor
+                'connect-src': "'self'", # Controla de onde o site por abrir conexões dinâmicas (somente do proprio servidor). Isso permite controle sobre quem meu servidor está se relacionando. Posso liberar acesso a servidores externos específicos e de confiança.
+                'object-src': "'none'", # Proibe plugins externos comuns que as individuos maliciosos utilizam para acessar o sistema.
+                'base-uri': "'self'",  # Redirecionamentos apenas para o próprio servidor
+                'frame-ancestors': "'none'" # Proibe que meu servidor seja carregado dentro de outro (inframes)
             },
-            referrer_policy='strict-origin-when-cross-origin'
+            referrer_policy='strict-origin-when-cross-origin' # Controla o acesso de informações quando você navega por outros servidores evitando que eles acessem informações importantes. Pode mostrar o site que vc estava anteriormente, mas não mostra mais nada a respeito das rotas que vc acessou nele, qual usuário vc é, etc... ou seja, o servidor externo só tem acesso a: ele estava em "meusite.com" e não a: "meusite.com/user_id=1/pay_value=...."
             )
 
 else:
     # CONFIGURAÇÃO PARA DESENVOLVIMENTO (flexível)
     Talisman(app,
         force_https=False,  # Não força HTTPS no localhost
-        strict_transport_security=False,  # Desabilita HSTS no desenvolvimento
+        strict_transport_security=False,  # Desabilita o redirecionamento para HTTPS caso tente por outra via
         content_security_policy={
-            'default-src': "'self'",
-            'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",  # Mais permissivo para dev
-            'style-src': "'self' 'unsafe-inline'",
-            'img-src': "'self' data:",
+            'default-src': "'self'", # Continua aceitando recursos somente do proprio servidor
+            'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",  # Permite scripts externos em inline(código js direto no html) e eval(), que transforma textos em códigos java script que auxilia na detecção de erros no sistema
+            'style-src': "'self' 'unsafe-inline'", # Permenece igual ao de produção, permitindo css direto no html
+            'img-src': "'self' data:", # Permanece igual, permitindo imagens carregadas, e também aquelas já processadas direto no html
         }
     )
 
 @app.after_request
 def after_request(response):
-    """Headers de segurança aplicados em TODAS as rotas"""
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    response.headers['X-Frame-Options'] = 'DENY'
-    response.headers['X-XSS-Protection'] = '1; mode=block'
+    """Headers de segurança aplicados em TODAS as rotas""" # São tipo placas de regras de segurança que o navagador idêntifica para poder gerenciar o acesso ao seu servidor
+    response.headers['X-Content-Type-Options'] = 'nosniff' # O navegador não está nem ai para qualquer arquivo que venha de fora, nem advinha o tipo (.js, .py, .css, etc...)
+    response.headers['X-Frame-Options'] = 'DENY' # Não pode colocar seu site dentro de outro (inframe)
+    response.headers['X-XSS-Protection'] = '1; mode=block' # Bloqueia scripts maliciosos, sendo efetivo contra ataques XSS
     
     # Em produção, adicionar headers extras
     if IS_PRODUCTION:
-        response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+        response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin' # Protege a privaciadade do usuario de sua página, não revelando a outros servidores de qual site ele veio
         # Remove headers que vazam informações
-        response.headers.pop('Server', None)
+        response.headers.pop('Server', None) # Impede usuários maliciosos de saber detalhes acerca de meu servidor 
     
     return response
 
--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,11 +1,46 @@
+# OS será chamdado para manipulações necessárias no sistema operacional
+import os
+# Importações padrões do Flask
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session
 from flask_login import login_user, login_required, logout_user, current_user
 from models.admnistrador_log import AdministradorLog
 # Ele foi importado de extensions para evitar problemas de importação circular
 from security.forms_security import LoginForm
 from security.extensions import limiter
+# Essa é uma tag para usar futuramente para os pedidos de oração que a comunidade irá realizar para evitar ataques com <scripit>
+import bleach
+# A seguinte biblioteca é uma implementação de um sistema próprio que ela oferece para registro de logs no sistema
+# Ela será utilizada para verificar atividades de login. Ajuda também no processo de depuração
+import logging
+import logging.handlers
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+# Configurando a biblioteca de segurança para logs
+# Garantindo que a pasta sempre exista
+os.makedirs('security/login_security', exist_ok=True)
+
+# Criando loggers específico para segurança que não intervenham ou afetem outros logs
+security_logger = logging.getLogger('security')
+# Configurando o nível de captura para capturar sucessos no login
+security_logger.setLevel(logging.INFO)
+
+# Configuração de handlers para evitar superlotação da memória com logs 
+handler = logging.handlers.RotatingFileHandler(
+    "security/login_security/security.log",
+    maxBytes=5*1024*1024, # Cada arquivo com no máximo 5MB
+    backupCount=3, # Apenas 3 arquivos antigos permancem quando o limite de 5 arquivos for atingido
+    encoding='utf-8' # Funciona melhor com os diferentes tipos de letras, especialmente no latin
+)
+
+# Formatação personalizada dos registro
+formatter = logging.Formatter(
+    '%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+) 
+handler.setFormatter(formatter)
+security_logger.addHandler(handler)
 
 
 # Rota para login
@@ -16,9 +51,9 @@ def logar_no_sistema():
     # Resolvi realizar um formulário efetivamente seguro. Para não sujar meu código, coloquei ele dentro da pasta de segurança e importei ele aqui.
     formulario = LoginForm()
     if request.method == 'POST':
-        # Pegando os dados do formulário
+        # Pegando os dados do formulário e limpando possíveis scripts maliciosos com a biblioteca bleach
         admin_email = formulario.email.data
-        admin_password = formulario.password.data
+        admin_password = formulario.password.data 
 
         # Verificando a sua validação
         if formulario.validate_on_submit():
@@ -26,8 +61,12 @@ def logar_no_sistema():
             administrador = AdministradorLog.get_by_email(admin_email)
             if administrador and administrador.check_password(admin_email, admin_password) and administrador.ADMIN_ACTIVE==True:
                 
-                # Variaveis para dados especiais que ó podem ser utilizados por um usuário root
+                # Para que adm possa ter acesso ao sistema
                 login_user(administrador)
+
+                # Registando o registro efetivo de log de segurança 
+                security_logger.info(f'LOGIN_SUCESS: {admin_email} from {request.remote_addr}')
+
                 if administrador.is_root():
                     session['dados_sensiveis'] = "Aaaaaah, estou chorando :("
                     return redirect(url_for('administrador.index'))
@@ -35,6 +74,8 @@ def logar_no_sistema():
                     return redirect(url_for('administrador.index'))
 
             else:
+                # Registrando tentativa falha de login no log de segurança
+                security_logger.warning(f'LOGIN_FAILURE: {admin_email} from {request.remote_addr}')
                 flash ('Email ou senha incorretos', 'danger')
         
             
