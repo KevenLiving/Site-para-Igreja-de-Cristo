f4cb936e6465e0481a7a3ca82e20cb46dda5a523
Licurgo Keven Medeiros Cavalcanti <20221101110050@ifrn.local>
2025-11-11 14:24:35 -0300

    Criação automática de usuário root

--- models/database.py
+++ models/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.orm import DeclarativeBase
+import os
 
-
+secret_key = os.environ.get('chave_teste_base64').encode()
 
 class Base(DeclarativeBase):
     pass
@@ -21,6 +22,28 @@ def init_db():
     # Importações prévias
     from models.administrador import Administrador
     from models.admnistrador_log import AdministradorLog
+
+    # Verificando a existencia de um usuário root 
+    with SessionFactory() as session:
+        root = session.query(Administrador).filter_by(ADMIN_ROLE='root').first()
+        if not root:
+            # Importando a senha de usuário root
+            secret_key = os.environ.get('senha_root')
+            # Importando email root
+            email = os.environ.get('email_root')
+
+            # Verificando a existencia das variáveis
+            if secret_key and email:
+                # Criando objeto
+                user_root = Administrador(ADMIN_NAME = 'root', ADMIN_EMAIL = email, ADMIN_ROLE = 'root', ADMIN_ACTIVE = True)
+                user_root.set_password(secret_key)
+                session.add(user_root)
+                session.commit()
+            else:
+                print('❌ Falha nas variáveis de usuário root, verifique se elas foram declaradas ou crie o user manualmente')
+
+        else:
+            pass
     
 
 
